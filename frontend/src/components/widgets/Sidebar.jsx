27d4fbfd12806b95c662575ea553ef7d46d4dd9f
Ioan Solovastru <nelusolovastru@gmail.com>
2023-05-05 11:55:42 +0300

    simplify frontend

--- frontend/src/components/widgets/Sidebar.jsx
+++ frontend/src/components/widgets/Sidebar.jsx
@@ -1,74 +1,39 @@
-import React from 'react';
-import Text from '../elements/Text';
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import Text from "../elements/Text";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <aside className="text-white">
-        <ul>
-            <Text className="text-lg pl-4 mt-6 font-bold mb-12">
-                Focus <span className="text-tertiary">App</span>
-            </Text>
+      <ul>
+        <Text className="text-lg pl-4 mt-6 font-bold mb-12">
+          Cloud<span className="text-tertiary"> Identity</span>
+        </Text>
 
-          
-                <NavLink 
-                    to="/home"
-                    className={({isActive}) => 
-                        isActive? "bg-secondary w-full block border-l-2 border-l-tertiary mr-2 py-3  text-sm"
-                        : 
-                        "mr-2 text-sm py-3 "
-                    }    
-                >                    
-                    <li className="p-4 ">
-                        Home
-                    </li>
-                </NavLink>
-            
-                <NavLink 
-                    to="/notes"
-                    className={({isActive}) => 
-                        isActive? "bg-secondary w-full block border-l-2 border-l-tertiary mr-2 py-3  text-sm"
-                        : 
-                        "mr-2 text-sm py-3 pl-4"
-                    }     
-                >
-                    <li className="p-4">
-                        Notes
-                    </li>                    
-                </NavLink>
-           
+        <NavLink
+          to="/home"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-secondary w-full block border-l-2 border-l-tertiary mr-2 py-3  text-sm"
+              : "mr-2 text-sm py-3 "
+          }
+        >
+          <li className="p-4 ">Home</li>
+        </NavLink>
 
-                <NavLink 
-                    to="/stopwatch"
-                    className={({isActive}) => 
-                        isActive? "bg-secondary w-full block border-l-2 border-l-tertiary mr-2 py-3  text-sm"
-                        : 
-                        "mr-2 text-sm py-3 pl-4"
-                    }     
-                >
-                    <li className="p-4">
-                        Stopwatch
-                    </li>
-                    
-                </NavLink>
-           
-            
-                <NavLink 
-                    to="/timer"
-                    className={({isActive}) => 
-                        isActive? "bg-secondary w-full block border-l-2 border-l-tertiary mr-2 py-3  text-sm"
-                        : 
-                        "mr-2 py-3 text-sm pl-4"
-                    }     
-                >
-                    <li className="p-4">
-                        Timer
-                    </li>                    
-                </NavLink>
-         
-        </ul>
+        <NavLink
+          to="/backend"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-secondary w-full block border-l-2 border-l-tertiary mr-2 py-3  text-sm"
+              : "mr-2 text-sm py-3 pl-4"
+          }
+        >
+          <li className="p-4">Backend</li>
+        </NavLink>
+      </ul>
     </aside>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
---
+++ frontend/src/page/Backend.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Backend = () => {
+  return <section className="text-white pt-10 pb-20">Backend</section>;
+};
+
+export default Backend;
